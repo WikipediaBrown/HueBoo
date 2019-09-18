2583e23d6e997ec4ecea67475ee3564e52a825b6
Wikipedia Brown <wikipediabrown@iamgoodbad.com>
2019-09-18 03:01:36 -0700

    Initial Commit

---
+++ HueBoo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HueBoo
+//
+//  Created by Wikipedia Brown on 9/18/19.
+//  Copyright © 2019 IamGoodBad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
